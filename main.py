e7fc071e0e10180ad7a2db51f69529ad3e21aa9e
mahendran-narayanan <mahendranNNM@gmail.com>
2023-10-11 16:30:23 +0530

    Alexnet model added

--- main.py
+++ main.py
@@ -13,25 +13,53 @@ class TfNN(tf.keras.Model):
 		super().__init__()
 		self.conv1 = tf.keras.layers.Conv2D(96,11,strides=4,activation='relu')
 		self.conv2 = tf.keras.layers.Conv2D(256,5,activation='relu')
+		self.conv3 = tf.keras.layers.Conv2D(384,3,activation='relu')
+		self.conv4 = tf.keras.layers.Conv2D(384,3,activation='relu')
+		self.conv5 = tf.keras.layers.Conv2D(256,3,activation='relu')
+		self.flat = tf.keras.layers.Flatten()
+		self.dense1 = tf.keras.layers.Dense(4096,activation='relu')
+		self.dense2 = tf.keras.layers.Dense(4096,activation='relu')
+		self.classifier = tf.keras.layers.Dense(1000,activation='sigmoid')
 
 	def call(self,x):
 		x = self.conv1(x)
 		x = self.conv2(x)
+		x = self.conv3(x)
+		x = self.conv4(x)
+		x = self.conv5(x)
+		x = self.flat(x)
+		x = self.dense1(x)
+		x = self.dense2(x)
+		x = self.classifier(x)
 		return x
 
-class TorchNN(torch.nn.Module):
+class Alexnet_torch(torch.nn.Module):
 	def __init__(self):
 		super().__init__()
 		self.conv1 = torch.nn.Conv2d(3, 96, kernel_size=11, stride=4)
 		self.conv2 = torch.nn.Conv2d(96, 256, kernel_size=5)
+		self.conv3 = torch.nn.Conv2d(256, 384, kernel_size=3)
+		self.conv4 = torch.nn.Conv2d(384, 384, kernel_size=3)
+		self.conv5 = torch.nn.Conv2d(256, 256, kernel_size=3)
+		self.flatten = torch.nn.Flatten()
+		self.dense1 = torch.nn.Linear(4096,4096)
+		self.dense2 = torch.nn.Linear(4096,4096)
+		self.classifier = torch.nn.Linear(4096,1000)
 		
 	def forward(self, x):
 		x = self.conv1(x)
 		x = self.conv2(x)
+		x = self.conv3(x)
+		x = self.conv4(x)
+		x = self.conv5(x)
+		x = self.flatten(x)
+		x = self.dense1(x)
+		x = self.dense2(x)
+		x = self.classifier(x)
 		return x
 
 def model_torch():
-	return TorchNN()
+	return Alexnet_torch()
 
 def model_tf():
 	return TfNN()
@@ -41,7 +69,7 @@ def main(args):
 	if args.model=='tf':
 		print('Model will be created in Tensorflow')
 		model = model_tf()
-		model.build(input_shape=(None,256,256,3))
+		model.build(input_shape=(None,224,224,3))
 		model.summary()
 	else:
 		print('Model will be created in Pytorch')
